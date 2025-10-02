44252089acadd23c74ef02bf0015d330dbf23239
Eiman John Espedido <eimanjohnespedido789@gmail.com>
2025-10-02 11:40:53 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,7 @@ import Home from "./pages/Home";
 import Leaderboards from "./pages/Leaderboards";
 import Settings from "./pages/Settings";
 
-const BASENAME = import.meta.env.BASE_URL;
-// Vite sets this from vite.config.ts "base" value
-
-const PAGES = [`${BASENAME}`, `${BASENAME}leaderboards`, `${BASENAME}settings`];
+const PAGES = ["/", "/leaderboards", "/settings"];
 
 export default function App() {
   return (
